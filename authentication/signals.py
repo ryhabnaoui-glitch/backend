2d9b7032eb8ff2dee2ae047952dbe228a08c0443
ryhabnaoui8-hash <ryhabnaoui8@gmail.com>
2025-09-13 16:55:02 +0100

    Update signals.py

--- authentication/signals.py
+++ authentication/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-
+// signals bach tcreer tokens lel login w registrations w api requests
 User = get_user_model()
 
 @receiver(post_save, sender=User)
