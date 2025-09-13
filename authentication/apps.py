e6214b3cb079960145e3e8c3983439d19a563286
ryhabnaoui8-hash <ryhabnaoui8@gmail.com>
2025-09-13 16:50:39 +0100

    Update apps.py

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
-
+// hadi juste fonction t3ayat lel signals
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
 
     def ready(self):
+
         import authentication.signals
